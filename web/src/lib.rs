47f54c9aa9a85cdbac051345490917ace52c9893
alostsock <49344439+alostsock@users.noreply.github.com>
2022-03-22 09:42:18 -0400

    for web, manually add recipe type

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,9 +1,37 @@
 #![allow(clippy::unused_unit)]
 use crafty::recipes::recipes_by_level;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsCast};
+
+#[wasm_bindgen(typescript_custom_section)]
+const TS_APPEND_CONTENT: &'static str = r#"
+
+export interface Recipe {
+    recipe_level: number,
+    job_level: number,
+    stars: number,
+    progress: number,
+    quality: number,
+    durability: number,
+    progress_div: number,
+    progress_mod: number,
+    quality_div: number,
+    quality_mod: number,
+    is_expert: boolean,
+    conditions_flag: number,
+}
+
+"#;
 
 #[wasm_bindgen]
-pub fn recipes(level: u32) -> JsValue {
+extern "C" {
+    #[wasm_bindgen(typescript_type = "Recipe[]")]
+    pub type RecipeArray;
+}
+
+#[wasm_bindgen]
+pub fn recipes(level: u32) -> RecipeArray {
     let recipes = recipes_by_level(level);
-    JsValue::from_serde(recipes).unwrap()
+    JsValue::from_serde(recipes)
+        .unwrap()
+        .unchecked_into::<RecipeArray>()
 }
