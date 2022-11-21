cd3495b8f8b260ab995f355deb893954752b5b9c
alostsock <hi@alostsock.dev>
2022-11-20 23:47:08 -0500

    use non-strict moveset when manually crafting

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -64,10 +64,11 @@ pub fn simulate_actions(
 
     let mut sim = Simulator::new(&recipe, &player, options);
     let (result_index, craft_result) = sim.execute_actions(0, actions);
-    let result_node = sim.tree.get(result_index);
+    let mut craft_state = sim.tree.get(result_index).state.clone();
+    craft_state.set_available_moves(false);
 
     let sim_result = SimulatorResult {
-        craft_state: result_node.state.clone(),
+        craft_state,
         completion_reason: match craft_result {
             Some(CraftResult::Finished(_)) => Some(CompletionReason::Finished),
             Some(CraftResult::DurabilityFailure) => Some(CompletionReason::DurabilityFailure),
