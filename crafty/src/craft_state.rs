92e5f9c76c460f20545423716e0961217eb2f22b
alostsock <hi@alostsock.dev>
2024-06-22 23:14:35 -0400

    remove maker's mark from buffs
    why was this even here?

--- crafty/src/craft_state.rs
+++ crafty/src/craft_state.rs
@@ -24,7 +24,6 @@ pub struct Buffs {
     pub great_strides: u8,
     pub innovation: u8,
     pub veneration: u8,
-    pub makers_mark: u8,
     pub muscle_memory: u8,
 }
 
@@ -42,7 +41,6 @@ impl Buffs {
         self.great_strides = self.great_strides.saturating_sub(1);
         self.innovation = self.innovation.saturating_sub(1);
         self.veneration = self.veneration.saturating_sub(1);
-        self.makers_mark = self.makers_mark.saturating_sub(1);
         self.muscle_memory = self.muscle_memory.saturating_sub(1);
     }
 }
